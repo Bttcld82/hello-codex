eb0352d96cfdaa4e5b387db5fb6609baeef5c064
Bttcld82 <bttcld82@gmail.com>
2025-10-03 12:42:58 +0200

    removed app-flask

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Entrypoint for running the Flask development server."""
 from __future__ import annotations
 
-from app_flask import create_app
+from app import create_app
 
 app = create_app()
 
